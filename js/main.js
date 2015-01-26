c0562c85337f8294348425f18cd9bb4e6d651174
sdw113322 <sdw113322@gmail.com>
2015-01-26 19:27:01 +0800

    fix modal problem in webkit engine (chrome, opera, safari)

--- js/main.js
+++ js/main.js
@@ -758,44 +758,7 @@ function addMonster(no,times,box)
 	$("#add input[name='quantity']").val("1");
 	$(".material-display").tooltipster(); //active tooltipster
 } 
-function adjustModalMaxHeightAndPosition(){
-    $('.modal').each(function(){
-        if($(this).hasClass('in') == false){
-            $(this).show(); /* Need this to get modal dimensions */
-        };
-        var contentHeight = $(window).height() - 60;
-        var headerHeight = $(this).find('.modal-header').outerHeight() || 2;
-        var footerHeight = $(this).find('.modal-footer').outerHeight() || 2;
-
-        $(this).find('.modal-content').css({
-            'max-height': function () {
-                return contentHeight;
-            }
-        });
-
-        $(this).find('.modal-body').css({
-            'max-height': function () {
-                return (contentHeight - (headerHeight + footerHeight));
-            }
-        });
-
-        $(this).find('.modal-dialog').addClass('modal-dialog-center').css({
-            'margin-top': function () {
-                return -($(this).outerHeight() / 2);
-            },
-            'margin-left': function () {
-                return -($(this).outerWidth() / 2);
-            }
-        });
-        if($(this).hasClass('in') == false){
-            $(this).hide(); /* Hide modal */
-        };
-    });
-};
 $(document).ready(function() {
-	if ($(window).height() >= 320){
-		$(window).resize(adjustModalMaxHeightAndPosition).trigger("resize");
-	}
 	window.localStorage.removeItem("settingA");
 	if(window.localStorage.getItem("setting") === null){
 		var setting = new Array();
