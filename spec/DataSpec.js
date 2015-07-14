4af00b9bc6753a6e4d4f19a8f4cb54a0ef28ac7c
vrabe <git@vrabe.tw>
2015-07-14 21:22:14 +0800

    fix

--- spec/DataSpec.js
+++ spec/DataSpec.js
@@ -4,6 +4,9 @@ describe("Data", function() {
 	});
 	it("新資料",function(){
 		Data.load();
-		expect(localStorage.padboxer_main).toBeDefined();
+		var entry = JSON.parse(localStorage.padboxer_main);
+		expect(entry.name).toBe("主帳號");
+		expect(entry.boxid).toEqual(0);
+		expect(entry.box).toBe([]);
 	});
 });
