fa5da12cac5c219f687cf67bbd35407007803829
vrabe <sdw113322@gmail.com>
2015-07-15 02:42:29 +0800

    fix

--- spec/DataSpec.js
+++ spec/DataSpec.js
@@ -53,5 +53,44 @@ describe("Data", function() {
 	it("切換資料",function(){
 		Data.switchEntry("2");
 		expect(Data.get("name")).toBe("2");
+		expect(Data.get("boxid")).toEqual(0);
+	});
+	it("取得資料",function(){
+		expect(Data.get("name")).toBe("2");
+		expect(Data.get("boxid")).toEqual(0);
+		expect(Data.get("box")).toEqual([]);
+		expect(Data.get("material")).toEqual([{"no":147,"quantity":0},{"no":148,"quantity":0},{"no":149,"quantity":0},{"no":150,"quantity":0},{"no":151,"quantity":0},{"no":321,"quantity":0},{"no":1176,"quantity":0},{"no":161,"quantity":0},{"no":171,"quantity":0},{"no":166,"quantity":0},{"no":162,"quantity":0},{"no":172,"quantity":0},{"no":167,"quantity":0},{"no":1294,"quantity":0},{"no":163,"quantity":0},{"no":173,"quantity":0},{"no":168,"quantity":0},{"no":1295,"quantity":0},{"no":164,"quantity":0},{"no":174,"quantity":0},{"no":169,"quantity":0},{"no":165,"quantity":0},{"no":175,"quantity":0},{"no":170,"quantity":0},{"no":234,"quantity":0},{"no":152,"quantity":0},{"no":153,"quantity":0},{"no":154,"quantity":0},{"no":227,"quantity":0},{"no":1085,"quantity":0},{"no":1086,"quantity":0},{"no":1087,"quantity":0},{"no":155,"quantity":0},{"no":156,"quantity":0},{"no":157,"quantity":0},{"no":158,"quantity":0},{"no":159,"quantity":0},{"no":160,"quantity":0},{"no":246,"quantity":0},{"no":247,"quantity":0},{"no":248,"quantity":0},{"no":249,"quantity":0},{"no":250,"quantity":0},{"no":251,"quantity":0},{"no":915,"quantity":0},{"no":916,"quantity":0},{"no":1325,"quantity":0},{"no":1326,"quantity":0},{"no":1327,"quantity":0},{"no":1328,"quantity":0},{"no":1329,"quantity":0}]);
+	});
+	it("編輯資料",function(){
+		Data.edit("name","the other");
+		Data.edit("boxid",65535);
+		expect(Data.get("name")).toBe("the other");
+		expect(Data.get("boxid")).toEqual(65535);
+	});
+	it("變更名稱",function(){
+		Data.rename("主帳號","Main");
+		Data.switchEntry("Main");
+		expect(Data.get("boxid")).toEqual(1);
+		expect(Data.get("box")).toEqual([{"id":"0","no":"2","priority":0,"quantity":"1"}]);
+		Data.switchEntry("the other");
+	});
+	it("刪除資料",function(){
+		Data.deleteEntry("the other");
+		Data.deleteEntry("Main");
+		expect(Data.get("name")).toBe("the other");
+		expect(localStorage.padboxer_current).toBe("the other");
+		var entry = JSON.parse(localStorage.padboxer_main);
+		expect(entry[0].name).toBe("the other");
+	});
+	//以上相依
+	it("還原資料",function(){
+		expect(Data.restore.bind(null,"fgjighi")).toThrow();
+		Data.restore("[{\"name\":\"主帳號\",\"version\":\"2.5\",\"boxid\":1,\"box\":[{\"id\":\"0\",\"no\":\"2\",\"priority\":0,\"quantity\":\"1\"}],\"material\":[{\"no\":147,\"quantity\":1},{\"no\":148,\"quantity\":1},{\"no\":149,\"quantity\":1},{\"no\":150,\"quantity\":0},{\"no\":151,\"quantity\":0},{\"no\":321,\"quantity\":0},{\"no\":1176,\"quantity\":0},{\"no\":161,\"quantity\":0},{\"no\":171,\"quantity\":0},{\"no\":166,\"quantity\":0},{\"no\":162,\"quantity\":0},{\"no\":172,\"quantity\":0},{\"no\":167,\"quantity\":0},{\"no\":1294,\"quantity\":0},{\"no\":163,\"quantity\":0},{\"no\":173,\"quantity\":0},{\"no\":168,\"quantity\":0},{\"no\":1295,\"quantity\":0},{\"no\":164,\"quantity\":0},{\"no\":174,\"quantity\":0},{\"no\":169,\"quantity\":0},{\"no\":165,\"quantity\":0},{\"no\":175,\"quantity\":0},{\"no\":170,\"quantity\":0},{\"no\":234,\"quantity\":0},{\"no\":152,\"quantity\":0},{\"no\":153,\"quantity\":0},{\"no\":154,\"quantity\":0},{\"no\":227,\"quantity\":0},{\"no\":1085,\"quantity\":0},{\"no\":1086,\"quantity\":0},{\"no\":1087,\"quantity\":0},{\"no\":155,\"quantity\":0},{\"no\":156,\"quantity\":0},{\"no\":157,\"quantity\":0},{\"no\":158,\"quantity\":0},{\"no\":159,\"quantity\":0},{\"no\":160,\"quantity\":0},{\"no\":246,\"quantity\":0},{\"no\":247,\"quantity\":0},{\"no\":248,\"quantity\":0},{\"no\":249,\"quantity\":0},{\"no\":250,\"quantity\":0},{\"no\":251,\"quantity\":0},{\"no\":915,\"quantity\":0},{\"no\":916,\"quantity\":0},{\"no\":1325,\"quantity\":0},{\"no\":1326,\"quantity\":0},{\"no\":1327,\"quantity\":0},{\"no\":1328,\"quantity\":0},{\"no\":1329,\"quantity\":0}],\"setting\":[false,false,null,[true,true,true,true,true,true,true,true,true],false,0]}]");
+		var entry = JSON.parse(localStorage.padboxer_main);
+		expect(entry[0].name).toBe("主帳號");
+		expect(entry[0].boxid).toEqual(1);
+		expect(entry[0].box).toEqual([{"id":"0","no":"2","priority":0,"quantity":"1"}]);
+		expect(entry[0].material).toEqual([{"no":147,"quantity":1},{"no":148,"quantity":1},{"no":149,"quantity":1},{"no":150,"quantity":0},{"no":151,"quantity":0},{"no":321,"quantity":0},{"no":1176,"quantity":0},{"no":161,"quantity":0},{"no":171,"quantity":0},{"no":166,"quantity":0},{"no":162,"quantity":0},{"no":172,"quantity":0},{"no":167,"quantity":0},{"no":1294,"quantity":0},{"no":163,"quantity":0},{"no":173,"quantity":0},{"no":168,"quantity":0},{"no":1295,"quantity":0},{"no":164,"quantity":0},{"no":174,"quantity":0},{"no":169,"quantity":0},{"no":165,"quantity":0},{"no":175,"quantity":0},{"no":170,"quantity":0},{"no":234,"quantity":0},{"no":152,"quantity":0},{"no":153,"quantity":0},{"no":154,"quantity":0},{"no":227,"quantity":0},{"no":1085,"quantity":0},{"no":1086,"quantity":0},{"no":1087,"quantity":0},{"no":155,"quantity":0},{"no":156,"quantity":0},{"no":157,"quantity":0},{"no":158,"quantity":0},{"no":159,"quantity":0},{"no":160,"quantity":0},{"no":246,"quantity":0},{"no":247,"quantity":0},{"no":248,"quantity":0},{"no":249,"quantity":0},{"no":250,"quantity":0},{"no":251,"quantity":0},{"no":915,"quantity":0},{"no":916,"quantity":0},{"no":1325,"quantity":0},{"no":1326,"quantity":0},{"no":1327,"quantity":0},{"no":1328,"quantity":0},{"no":1329,"quantity":0}]);
+		expect(entry[0].setting).toEqual([false,false,null,[true,true,true,true,true,true,true,true,true],false,0]);
 	});
 });
