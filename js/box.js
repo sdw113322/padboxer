babe40820e0e9cdd2202e5b30dda3146be9ed722
vrabe <sdw113322@gmail.com>
2015-07-23 21:31:44 +0800

    add

--- js/box.js
+++ js/box.js
@@ -45,7 +45,56 @@ var Box = (function() {
 			Data.edit("box",box);
 		},
 		evolution: function(id){
-			
+			var monster = box[id];
+			var evol = Index.get(monster.no,"evolution");
+			var need = [];
+			var result = {};
+			//找需要的素材及進化結果
+			switch(evol.status){
+				case "n":
+				case "?":
+				throw new Error("不能進化");
+				break;
+				case "y":
+				need = evol.need;
+				result = evol.result;
+				break;
+				case: "u":
+				if(typeof monster.choice === "undefined")
+					throw new Error("未指定究極進化選項");
+				var branches = Index.get(monster.no,"ultimate");
+				for(var i in branches)
+					if(Number(branches[i].result) === Number(monster.choice)){
+						need = branches[i].need;
+						result = branches[i].result;
+					}
+			}
+			//確認是否有足夠素材
+			var mater = { };
+			var notEnough = [];
+			for(i = 0; i < need.length; ++i) {
+				if(!mater[need[i]])
+					mater[need[i]] = 0;
+				++mater[need[i]];
+			}
+			for(var j in mater){
+				if(Material.include(j) === true)
+					if(Material.quantity(j) < mater[j])
+						notEnough.push(j);
+			}
+			if(notEnough.length > 0){
+				var errmsg = "缺少下列素材:";
+				for(var k in notEnough){
+					errmsg += notEnough[k];
+					errmsg += " - ";
+					errmsg += Index.get(notEnough[k],"name").chinese;
+					errmsg +=  "\n";
+				}
+				throw new Error(errmsg);
+			}
+			//修改素材數量
+			var prior = (monster.priority > 0);
+			Material.evolution(monster.no,monster.choice,prior);
 		}
 	}
 }());
--- js/material.js
+++ js/material.js
@@ -1,63 +1,42 @@
 var Material = (function() {
 	var materials = [];
 	function countMaterial() {
-		var evolution = JSON.parse(window.localStorage.evolution);
-		var ultimate = JSON.parse(window.localStorage.ultimate);
-		var box = dataLoad("box");
-		var allNeed = [];
-		var PAllNeed = [];
+		var evolution = {};
+		var ultimate = {};
+		var box = Box.allMonsters();
+		var need = [];
+		var allNeed = {};
+		var PAllNeed = {};
 		for(var i in box){
-			var text =  box[i].no;
-			var choice =  box[i].choice;
-			var priority = box[i].priority;
-			var quantity = box[i].quantity;
-			if(text in evolution){
-				if(evolution[text].status == 'y'){
-					var j = 0;
-					for(j=0;j<evolution[text].need.length;j++){
-						if(evolution[text].need[j] in allNeed){
-							allNeed[evolution[text].need[j]] += Number(quantity);
-						}else{
-							allNeed[evolution[text].need[j]] = Number(quantity);
-						}
-						if(priority>0)
-							if(evolution[text].need[j] in PAllNeed)
-								PAllNeed[evolution[text].need[j]] += Number(priority);
-							else
-								PAllNeed[evolution[text].need[j]] = Number(priority);
+			var monster = Box.get(box[i]);
+			evolution = Index.get(monster.no,"evolution");
+			if(evolution.status === "y"){
+				need = evolution.need;
+			}else if(evolution.status === "u" && typeof monster.choice !== "undefined"){
+				var branches = Index.get(id,"ultimate");
+				for(var i in branches)
+					if(Number(branches[i].result) === Number(monster.choice)){
+						need = branches[i].need;
 					}
-				}
-				else if(evolution[text].status == 'u'){
-					if(choice > 0){
-						var k = 1;
-						var ultimateNeed = ultimate[k].need;
-						while(ultimate[k].result!=choice){
-							k++;
-							ultimateNeed = ultimate[k].need;
-						}
-						for(j=0;j<ultimateNeed.length;j++){
-							if(ultimateNeed[j] in allNeed){
-								allNeed[ultimateNeed[j]] += Number(quantity);
-							}else{
-								allNeed[ultimateNeed[j]] = Number(quantity);
-							}
-							if(priority>0)
-								if(ultimateNeed[j] in PAllNeed)
-									PAllNeed[ultimateNeed[j]] += Number(priority);
-								else
-									PAllNeed[ultimateNeed[j]] = Number(priority);
-						}
-					}
-				}
 			}
-			for(var i in materials){
-				var need = allNeed[materials[i].no];
-				var Pneed = PAllNeed[materials[i].no];
-				if(need != undefined)
-					materials[i].need = need;
-				if(Pneed != undefined)
-					materials[i].Pneed = Pneed;
+			for(var j in need){
+				if(typeof allNeed[need[j]] === "undefined" )
+					allNeed[need[j]] = monster.quantity;
+				else
+					allNeed[need[j]] += monster.quantity;
+				if(typeof PallNeed[need[j]] === "undefined" )
+					PallNeed[need[j]] = monster.quantity;
+				else
+					PallNeed[need[j]] += monster.quantity;
 			}
+		}
+		for(var k in materials){
+			var need = allNeed[materials[k].no];
+			var Pneed = PAllNeed[materials[k].no];
+			if(need != undefined)
+				materials[k].need = need;
+			if(Pneed != undefined)
+				materials[k].Pneed = Pneed;
 		}
 	}
     function transform( no ) { 
@@ -66,21 +45,8 @@ var Material = (function() {
 			if(materialAttr[key].no == no)
 				x = key;
 		}
-		if(x === -1)
-			return [0,false];
-		else
-			return [x, true];
+		return x;
     }
-	function change( id , mode ) {
-		if(mode === "normal"){
-			$("#material button[data-id='" + id + "']").parent().parent().parent().children().eq(3).text(materials[id].quantity);
-			$("#material button[data-id='" + id + "']").parent().parent().parent().children().eq(4).text(materials[id].need);
-			$("#material button[data-id='" + id + "']").parent().parent().parent().children().eq(5).text(materials[id].quantity - materials[id].need);
-		}else if(mode ==="priority"){
-			$("#material button[data-id='" + id + "']").parent().parent().parent().children().eq(6).text(materials[id].Pneed);
-			$("#material button[data-id='" + id + "']").parent().parent().parent().children().eq(7).text(materials[id].quantity - materials[id].Pneed);
-		}
-	}
 	function getState( id ) {
 		var available = materials[id].quantity;
 		var total = materials[id].quantity - materials[id].need;
@@ -92,97 +58,63 @@ var Material = (function() {
 			return "notation-important";
 	}
     return {
-		init: function() {
-			var string = window.localStorage.getItem("material");
-			if(string === null){
-				string = "[{\"no\":147,\"quantity\":\"0\"},{\"no\":148,\"quantity\":0},{\"no\":149,\"quantity\":0},{\"no\":150,\"quantity\":0},{\"no\":151,\"quantity\":0},{\"no\":321,\"quantity\":0},{\"no\":1176,\"quantity\":0},{\"no\":161,\"quantity\":0},{\"no\":171,\"quantity\":0},{\"no\":166,\"quantity\":0},{\"no\":162,\"quantity\":0},{\"no\":172,\"quantity\":0},{\"no\":167,\"quantity\":0},{\"no\":1294,\"quantity\":0},{\"no\":163,\"quantity\":0},{\"no\":173,\"quantity\":0},{\"no\":168,\"quantity\":0},{\"no\":1295,\"quantity\":0},{\"no\":164,\"quantity\":0},{\"no\":174,\"quantity\":0},{\"no\":169,\"quantity\":0},{\"no\":165,\"quantity\":0},{\"no\":175,\"quantity\":0},{\"no\":170,\"quantity\":0},{\"no\":234,\"quantity\":0},{\"no\":152,\"quantity\":0},{\"no\":153,\"quantity\":0},{\"no\":154,\"quantity\":0},{\"no\":227,\"quantity\":0},{\"no\":1085,\"quantity\":0},{\"no\":1086,\"quantity\":0},{\"no\":1087,\"quantity\":0},{\"no\":155,\"quantity\":0},{\"no\":156,\"quantity\":0},{\"no\":157,\"quantity\":0},{\"no\":158,\"quantity\":0},{\"no\":159,\"quantity\":0},{\"no\":160,\"quantity\":0},{\"no\":246,\"quantity\":0},{\"no\":247,\"quantity\":0},{\"no\":248,\"quantity\":0},{\"no\":249,\"quantity\":0},{\"no\":250,\"quantity\":0},{\"no\":251,\"quantity\":0},{\"no\":915,\"quantity\":0},{\"no\":916,\"quantity\":0},{\"no\":1325,\"quantity\":0},{\"no\":1326,\"quantity\":0},{\"no\":1327,\"quantity\":0},{\"no\":1328,\"quantity\":0},{\"no\":1329,\"quantity\":0}]";
-				window.localStorage.material = string;
-			}
-			materials = JSON.parse(string);
-			if(materials.length==46){
-				materials[46] = JSON.parse("{\"no\":1325,\"quantity\":0}");
-				materials[47] = JSON.parse("{\"no\":1326,\"quantity\":0}");
-				materials[48] = JSON.parse("{\"no\":1327,\"quantity\":0}");
-				materials[49] = JSON.parse("{\"no\":1328,\"quantity\":0}");
-				materials[50] = JSON.parse("{\"no\":1329,\"quantity\":0}");
-			}
-			for(var i=0;i<51;i++){
-				materials[i].need = 0;
-				materials[i].Pneed = 0;
-			}
-			countMaterial();
-		},
-		init_new: function(){
+		load: function() {
 			materials = Data.get("material");
-			for(var i=0;i<51;i++){
-				materials[i].need = 0;
-				materials[i].Pneed = 0;
-			}
 			countMaterial();
 		},
-		needPlus: function( no , quantity ) {
-			quantity = Number(quantity);
-			var index = transform( no );
-			if(index[1] != false){
-				materials[index[0]].need += quantity;
-				change(index[0],"normal");
+		include: function(no){
+			var x = -1;
+			for(var key in materialAttr){
+				if(materialAttr[key].no == no)
+					x = key;
+			}
+			if(x === -1)
+				return false;
+			else
+				return true;
+		},
+		boxEdit: function(no,quantity,choice,prior){
+			var res = Index.getMaterials(no,choice);
+			if(prior === true){
+				for(var i in res.need){
+					if(Material.include(res.need[i]) === true)
+						materials[transform(res.need[i])].Pneed += quantity;
+				}
+			}else{
+				for(var j in res.need){
+					if(Material.include(res.need[j]) === true)
+						materials[transform(res.need[j])].need += quantity;
+				}
 			}
 		},
-		needMinus: function( no , quantity ) {
-			quantity = Number(quantity);
-			var index = transform( no );
-			if(index[1] != false){
-				materials[index[0]].need -= quantity;
-				change(index[0],"normal");
-			}
+		drift: function(id,offset){
+			materials[id].quantity += offset;
 		},
-		quantityPlus: function(id){
-			materials[id].quantity ++;
-		},
-		quantityMinus: function(id){
-			materials[id].quantity --;
-		},
-		setQuantity: function(id,quantity){
-			quantity = Number(quantity);
+		edit: function(id,quantity){
 			materials[id].quantity = quantity;
 		},
-		evolution: function( no , quantity ) {
-			quantity = Number(quantity);
-			var index = transform( no );
-			if(index[1] != false){
-				materials[index[0]].quantity -= quantity;
-				materials[index[0]].need -= quantity;
-				change(index[0],"normal");
-			}
-		},
-		PneedPlus: function( no , quantity ) {
-			quantity = Number(quantity);
-			var index = transform( no );
-			if(index[1] != false){
-				materials[index[0]].Pneed += quantity;
-				change(index[0],"priority");
-			}
-		},
-		PneedMinus: function( no , quantity ) {
-			quantity = Number(quantity);
-			var index = transform( no );
-			if(index[1] != false){
-				materials[index[0]].Pneed -= quantity;
-				change(index[0],"priority");
-			}
-		},
-		Pevolution: function( no , quantity ) {
-			quantity = Number(quantity);
-			var index = transform( no );
-			if(index[1] != false){
-				materials[index[0]].Pneed -= quantity;
-				change(index[0],"priority");
+		evolution: function(no,choice,prior){
+			var res = Index.getMaterials(no,choice);
+			if(prior === true){
+				for(var i in res.need){
+					if(Material.include(res.need[i]) === true){
+						materials[transform(res.need[i])].Pneed --;
+						materials[transform(res.need[j])].need --;
+						materials[transform(res.need[j])].quantity --;
+					}
+				}
+			}else{
+				for(var j in res.need){
+					if(Material.include(res.need[j]) === true){
+						materials[transform(res.need[j])].need --;
+						materials[transform(res.need[j])].quantity --;
+					}
+				}
 			}
 		},
 		state: function( no ) {
-			var index = transform( no );
-			if(index[1] != false)
-				return getState( index[0] );
+			if(Material.include(no) === true)
+				return getState( transform(no) );
 			else
 				return false;
 		},
@@ -202,35 +134,4 @@ var Material = (function() {
 				return materials[id].quantity - materials[id].Pneed;
 		}
     };
-})();
-
-function NeedMaterial( no , choice ){
-	var evolution = JSON.parse(window.localStorage.evolution);
-	var ultimate = JSON.parse(window.localStorage.ultimate);
-	var result = [];
-	var status;
-	if(no in evolution){
-		if(evolution[no].status == 'y'){
-			result = evolution[no].need;
-		}
-		else if(evolution[no].status == 'u'){
-			if(choice > 0){
-				var i = 1;
-				var ultimateNeed = ultimate[i].need;
-				while(ultimate[i].result!=choice){
-					i++;
-					ultimateNeed = ultimate[i].need;
-				}
-				result = ultimateNeed;
-			}
-		}
-		if(evolution[no].status == 'u' && choice == 0)
-			status = 'un';
-		else
-			status = evolution[no].status;
-		return{
-			result : result,
-			status : status
-		};
-	}else return false;
-}
+}());
